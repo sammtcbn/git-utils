cb74de4967e82f4ca5bbc4271e0da606587727a8
sammtcbn <sammtcbn@gmail.com>
2019-08-22 23:55:20 +0800

    setup script can work in WSL

--- win_setup.bat
+++ win_setup.bat
@@ -1,2 +1,10 @@
-xcopy /Y script_win\*.* C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+@rem old method , if files are in WSL, path will be C:\Windows , aka %cd%
+@rem So if files are in WSL, it will fail to copy because of wrong source path
+@rem xcopy /Y bin_for_win\*.* C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+@rem xcopy /Y git_script_for_win\*.* C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+
+@rem below method works if files are in WSL too.
+@set currdir=%~dp0
+@xcopy /Y %currdir%\script_win\*.* C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+
 pause
